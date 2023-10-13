034bc5a84a97e0400445f0822fdf7c1537b86554
권기완 <digh7169@gmail.com>
2023-10-13 14:23:10 +0900

    Fix: 병합 과정에서 생긴 문제 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,21 +4,27 @@ import './Login.scss';
 const Login = () => {
   const kakaoAuthUrl = process.env.REACT_APP_KAKAO_REST_API;
   const naverAuthUrl = process.env.REACT_APP_NAVER_REST_API;
+
   return (
-    <div className="login">
-      <div className="contentsWrap">
-        <a className="snsLogin" href={kakaoAuthUrl}>
-          <button className="snsBtn loginKakao">
-            <img src="/images/login_kakao.svg" alt="카카오 이미지" />
-            <strong>카카오</strong>로 로그인
-          </button>
-        </a>
-        <a className="snsLogin" href={naverAuthUrl}>
-          <button className="snsBtn loginNaver">
-            <img src="/images/login_naver.svg" alt="네이버 이미지" />
-            <strong>네이버</strong>로 로그인
-          </button>
-        </a>
+    <div className="login contentsWrap">
+      <div className="wrap">
+        <div className="logoWrap">
+          <img src="/images/logo_white.png" />
+        </div>
+        <div className="loginWrap">
+          <a className="snsLogin" href={kakaoAuthUrl}>
+            <span className="snsBtn loginKakao">
+              <img src="/images/login_kakao.svg" alt="카카오" />
+              <strong>카카오</strong>로 로그인
+            </span>
+          </a>
+          <a className="snsLogin" href={naverAuthUrl}>
+            <span className="snsBtn loginNaver">
+              <img src="/images/login_naver.svg" alt="네이버" />
+              <strong>네이버</strong>로 로그인
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/Login/LoginKakao.js
+++ src/pages/Login/LoginKakao.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const LoginKakao = () => {
-  const code = new URL(window.location.href).searchParams.get('code');
+  const [serchParams] = useSearchParams();
+  const code = serchParams.get('code');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -11,7 +12,6 @@ const LoginKakao = () => {
       axios
         .get(`http://10.58.52.78:4000/auth/kakao/callback?code=${code}`)
         .then(res => {
-          console.log(res);
           if (res.data.message === 'LOGIN_SUCCESS') {
             navigate('/');
           }
