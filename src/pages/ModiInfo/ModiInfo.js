bc3ead7097d49d24233256e61d003a69bfd30b38
권기완 <digh7169@gmail.com>
2023-10-16 17:09:20 +0900

    style : 컴포넌트 생성

---
+++ src/pages/ModiInfo/ModiInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ModiInfo.scss';
+
+const ModiInfo = () => {
+  return (
+    <div className="modiInfo contentsWrap">
+      <div></div>
+    </div>
+  );
+};
+
+export default ModiInfo;
