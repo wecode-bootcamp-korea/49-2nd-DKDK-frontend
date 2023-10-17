58107f053114098df11a8fb1f726bdaa1ceb1c6c
이영은 <hisenselee12@gmail.com>
2023-10-17 20:29:05 +0900

    [style, feat]트레이너매칭 ui구현, 무한스크롤 기능 추가

--- src/pages/Trainers/Trainers.js
+++ src/pages/Trainers/Trainers.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
+import TrainersGroup from './TrainersGroup/TrainersGroup';
 import './Trainers.scss';
 
 const Trainers = () => {
@@ -8,42 +9,34 @@ const Trainers = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   let offset = searchParams.get('offset');
-  let limit = searchParams.get('limit');
+  const limit = 6;
 
   if (offset === null) {
-    offset = 6;
+    offset = 0;
   }
 
-  const setPaginationParams = () => {
-    limit = 12;
-    searchParams.set('offset', page * 6);
-    searchParams.set('limit', page * 6);
-    setSearchParams(setSearchParams);
+  const setPaginationParams = nextOffset => {
+    searchParams.set('offset', nextOffset);
+    searchParams.set('limit', limit);
+    setSearchParams(searchParams);
   };
 
   useEffect(() => {
-    axios('https://dummyjson.com/products?offset=0&limit=6').then(
-      function (response) {
-        setPaginationParams();
-        setData(response.data.products);
-      },
-    );
-  }, []);
+    axios(
+      `https://dummyjson.com/products?offset=${offset}&limit=${limit}`,
+    ).then(function (response) {
+      setData(prevData => [...prevData, ...response.data.products]);
+    });
+  }, [offset, limit]);
 
   useEffect(() => {
     const handleScroll = () => {
       const { scrollTop, clientHeight, scrollHeight } =
         document.documentElement;
       if (scrollHeight - scrollTop === clientHeight) {
-        axios(
-          `https://dummyjson.com/products?offset=${offset}&limit=${limit}`,
-        ).then(function (response) {
-          setPaginationParams();
-          setData(response.data.products);
-          setPage(page + 1);
-          console.log('offset:', offset);
-          console.log(data);
-        });
+        const nextOffset = page * limit;
+        setPaginationParams(nextOffset);
+        setPage(prevPage => prevPage + 1);
       }
     };
 
@@ -70,35 +63,7 @@ const Trainers = () => {
           <option>활동 순</option>
         </select>
       </div>
-      <ul className="trainersWrap">
-        {data?.map((trainer, index) => {
-          return (
-            <li className="trainerItem" key={index}>
-              <div className="trainerImg">
-                <img
-                  src={process.env.PUBLIC_URL + '/images/logo_white.png'}
-                  alt="프로필 이미지"
-                />
-              </div>
-              <div className="trainerName">{trainer.user_name}</div>
-              <div className="trainerInfo">
-                <p className="infoHalf">
-                  <span className="bold">전공</span>: {trainer.major}
-                </p>
-                <div className="detailInfo">
-                  <p className="leftHalfWrap">
-                    <span className="bold">가격</span>
-                    {`: ${trainer.price.toLocaleString()}`}
-                  </p>
-                  <p className="halfWrap">
-                    <span className="bold">위치</span>: {trainer.available_area}
-                  </p>
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <TrainersGroup trainerListData={data} />
     </section>
   );
 };
