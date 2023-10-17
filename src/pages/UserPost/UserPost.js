1783e31a8da90dfaf0d9b553857c537005fe89bc
AnByungwoo <quddn9204@naver.com>
2023-10-17 23:29:05 +0900

    구독자커뮤니티 UI 구현중

---
+++ src/pages/UserPost/UserPost.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './UserPost.scss';
+
+const UserPost = () => {
+  return (
+    <div className="contentsWrap">
+      <div className="postList">
+        <div className="container">
+          <div className="post">
+            <div className="userPost">
+              <img src="/images/user.png" alt="user" />
+              <span className="userName">홍길동</span>;
+            </div>
+            <div className="content">
+              <span className="postContent">3팀입니다!!</span>
+            </div>
+          </div>
+          <div className="post">
+            <div className="userPost">
+              <img src="/images/user.png" alt="user" />
+              <span className="userName">홍길동</span>;
+            </div>
+            <div className="content">
+              <span className="postContent">3팀입니다!!</span>
+            </div>
+          </div>
+          <div className="post">
+            <div className="userPost">
+              <img src="/images/user.png" alt="user" />
+              <span className="userName">홍길동</span>;
+            </div>
+            <div className="content">
+              <span className="postContent">3팀입니다!!</span>
+            </div>
+          </div>
+          <div className="buttonWrapper">
+            <label>
+              <input type="checkbox" />
+              <span>내 글보기</span>
+            </label>
+
+            <button className="writeButton">글 쓰기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserPost;
