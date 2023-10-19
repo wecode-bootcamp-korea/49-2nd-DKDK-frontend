744edfdcf111fe319e0f6173a2ef141b095e78f0
AnByungwoo <quddn9204@naver.com>
2023-10-19 15:58:11 +0900

    구독자 커뮤니티 UI 완료, 기능구현예정

---
+++ src/pages/PostSignup/PostSignup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './PostSignup.scss';
+
+const PostSignup = () => {
+  return <div>게시물등록페이지</div>;
+};
+export default PostSignup;
--- src/pages/UserPost/UserPost.js
+++ src/pages/UserPost/UserPost.js
@@ -4,40 +4,78 @@ import './UserPost.scss';
 const UserPost = () => {
   return (
     <div className="contentsWrap">
-      <div className="postList">
-        <div className="container">
-          <div className="post">
-            <div className="userPost">
-              <img src="/images/user.png" alt="user" />
-              <span className="userName">홍길동</span>;
+      <div className="container">
+        <div className="section">
+          <div className="rowSection">
+            <div className="section">
+              <div className="firstMessage">
+                <img
+                  className="running"
+                  src="/images/running.jpg"
+                  alt="달리기"
+                />
+                <div className="bodyBox">
+                  <div className="title">
+                    <a href="./PostDetail">러닝에 대해서...</a>
+                  </div>
+                  <span className="postContent">
+                    첫째, 러닝을 하면 체지방이 줄고 근육량이 늘면서 체격이
+                    좋아질 뿐만 아니라 바이러스로부터 견뎌낼 수 있는 방어 체력이
+                    향상된다.
+                    <br /> 둘째, 다른 운동보다 스트레스 해소에 좋다. 누구나
+                    장거리를 한바탕 달리고 나면 상쾌해지는 기분을 느낄 수 있다.{' '}
+                    <br />
+                    셋째, 체중 감량에 효과적이다.
+                  </span>
+                  <div>
+                    <img src="/images/like.png" alt="좋아요" />
+                    {<span className="number">10</span> /*기능구현 예정 */}
+                    <img src="/images/comment.png" alt="댓글" />
+                    {<span className="number">5</span> /*기능구현 예정 */}
+                  </div>
+                </div>
+              </div>
+              <div className="secondMessage">
+                <img
+                  className="training"
+                  src="/images/training.jpg"
+                  alt="헬스트레이닝"
+                />
+                <div className="bodyBox">
+                  <div className="title">
+                    <a href="./PostDetail">웨이트 트레이닝의 중요성</a>
+                  </div>
+                  <span className="postContent">
+                    첫째, 부상의 위험에서 벗어날 수 있다. 혼자서 부정확한
+                    동작으로 무거운 무게를 들 경우 모든관절과 근육인대에 손상을
+                    줄 수 있다.
+                    <br />
+                    둘째, 운동습관의 형성이다. 체계적인관리와 꾸준한 코칭을 받을
+                    수 있다.
+                    <br />
+                    셋째, 시너지효과를 나타낼 수 있다. 대부분의 운동은 함께할 때
+                    더 큰 효과를 나타낸다.
+                  </span>
+                  <div>
+                    <img src="/images/like.png" alt="좋아요" />
+                    {
+                      <span className="number">
+                        10
+                      </span> /*기능구현 예정 위와 같음 */
+                    }
+                    <img src="/images/comment.png" alt="댓글" />
+                    {
+                      <span className="number">
+                        5
+                      </span> /*기능구현 예정 위와 같음  */
+                    }
+                  </div>
+                </div>
+                <button>
+                  <a href="./PostSignup">글쓰기</a>
+                </button>
+              </div>
             </div>
-            <div className="content">
-              <span className="postContent">3팀입니다!!</span>
-            </div>
-          </div>
-          <div className="post">
-            <div className="userPost">
-              <img src="/images/user.png" alt="user" />
-              <span className="userName">홍길동</span>;
-            </div>
-            <div className="content">
-              <span className="postContent">3팀입니다!!</span>
-            </div>
-          </div>
-          <div className="post">
-            <div className="userPost">
-              <img src="/images/user.png" alt="user" />
-              <span className="userName">홍길동</span>;
-            </div>
-            <div className="content">
-              <span className="postContent">3팀입니다!!</span>
-            </div>
-          </div>
-          <div className="buttonWrapper">
-            <label>
-              <input type="checkbox" />내 글보기
-            </label>
-            <button className="writeButton">글 쓰기</button>
           </div>
         </div>
       </div>
