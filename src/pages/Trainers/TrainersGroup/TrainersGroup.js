fb7902396ff20853535949522c85f831d53ed164
이영은 <hisenselee12@gmail.com>
2023-10-23 08:00:29 +0900

    [fix]수정사항 반영 완료

--- src/pages/Trainers/TrainersGroup/TrainersGroup.js
+++ src/pages/Trainers/TrainersGroup/TrainersGroup.js
@@ -4,11 +4,12 @@ import './TrainersGroup.scss';
 const TrainersGroup = ({ trainerListData }) => {
   return (
     <ul className="trainersWrap">
-      {trainerListData?.map((trainer, index) => {
+      {trainerListData.map((trainer, index) => {
+        const { thumbnail, title, category, price, brand } = trainer;
         return (
           <li className="trainerItem" key={index}>
             <div className="trainerImg">
-              {trainer.thumbnail ? (
+              {thumbnail ? (
                 <img src={trainer.thumbnail} alt="프로필 이미지" />
               ) : (
                 <img
@@ -17,18 +18,18 @@ const TrainersGroup = ({ trainerListData }) => {
                 />
               )}
             </div>
-            <div className="trainerName">{trainer.title}</div>
+            <div className="trainerName">{title}</div>
             <div className="trainerInfo">
               <p className="infoHalf">
-                <span className="bold">전공</span>: {trainer.category}
+                <span className="bold">전공</span>: {category}
               </p>
               <div className="detailInfo">
                 <p className="leftHalfWrap">
                   <span className="bold">가격</span>:&nbsp;
-                  {trainer.price.toLocaleString()}
+                  {price.toLocaleString()}
                 </p>
                 <p className="halfWrap">
-                  <span className="bold">위치</span>: {trainer.brand}
+                  <span className="bold">위치</span>: {brand}
                 </p>
               </div>
             </div>
