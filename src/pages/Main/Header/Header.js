d625c2ea9364f1eb772aa108687a7918288da888
이영은 <hisenselee12@gmail.com>
2023-10-24 22:28:29 +0900

    [feat]header, nav 기능 구현

--- src/pages/Main/Header/Header.js
+++ src/pages/Main/Header/Header.js
@@ -1,24 +1,101 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import LoginGroup from './LoginGroup/LoginGroup';
 import './Header.scss';
 
-// 페이지 생성 후 Link path 설정하기
-const PATH_NAV = [
-  { id: 1, name: '홈', path: '/' },
-  { id: 2, name: '커뮤니티', path: '' },
-  { id: 3, name: '트레이너 매칭', path: '/trainer' },
-  { id: 4, name: '구독하기', path: '' },
-];
-
 const Header = () => {
+  const navigate = useNavigate();
   const [isMouseIn, setIsMouseIn] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState('');
+  const [accessToken, setAccessToken] = useState('');
   const mouseInEvent = () => {
     setIsMouseIn(true);
   };
   const mouseOutEvent = () => {
     setIsMouseIn(false);
   };
+
+  const goCommunity = () => {
+    if (accessToken === '') {
+      window.confirm('로그인이 필요한 서비스입니다.');
+      navigate('/login');
+    } else {
+      if (isSubscribed === 'true') {
+        navigate('/communitylist');
+      } else {
+        window.confirm('구독이 필요한 서비스입니다.');
+        navigate('/pay');
+      }
+    }
+  };
+
+  const goTrainer = () => {
+    if (accessToken === '') {
+      window.confirm('로그인이 필요한 서비스입니다.');
+      navigate('/login');
+    } else {
+      if (isSubscribed === 'true') {
+        navigate('/trainer');
+      } else {
+        window.confirm('구독이 필요한 서비스입니다.');
+        navigate('/pay');
+      }
+    }
+  };
+
+  const goSubScribe = () => {
+    if (accessToken === '') {
+      window.confirm('로그인이 필요한 서비스입니다.');
+      navigate('/login');
+    } else {
+      navigate('/pay');
+    }
+  };
+
+  useEffect(() => {
+    const handleIsSubscribed = e => {
+      if (e.key === 'isSubscribed') {
+        if (e.newValue) {
+          setIsSubscribed(e.newValue);
+        } else {
+          setIsSubscribed('');
+        }
+      }
+    };
+    const handleAccessToken = e => {
+      if (e.key === 'accessToken') {
+        if (e.newValue) {
+          setAccessToken(e.newValue);
+        } else {
+          setAccessToken('');
+        }
+      }
+    };
+
+    window.addEventListener('storage', handleIsSubscribed);
+    window.addEventListener('storage', handleAccessToken);
+
+    return () => {
+      window.removeEventListener('storage', handleIsSubscribed);
+      window.removeEventListener('storage', handleAccessToken);
+    };
+  }, []);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const isSubscribed = localStorage.getItem('isSubscribed');
+    if (accessToken) {
+      setAccessToken(accessToken);
+    } else {
+      setAccessToken('');
+    }
+
+    if (isSubscribed) {
+      setIsSubscribed(isSubscribed);
+    } else {
+      setIsSubscribed('');
+    }
+  }, []);
 
   return (
     <header className="header">
@@ -31,15 +108,26 @@ const Header = () => {
         </Link>
         <nav className="navWrap">
           <ul className="navItemWrap">
-            {PATH_NAV.map(nav => {
-              return (
-                <li className="navItem" key={nav.id}>
-                  <Link to={nav.path} className="pathWrap">
-                    {nav.name}
-                  </Link>
-                </li>
-              );
-            })}
+            <li className="navItem">
+              <Link to="/" className="pathWrap">
+                홈
+              </Link>
+            </li>
+            <li className="navItem">
+              <button type="button" className="pathBtn" onClick={goCommunity}>
+                커뮤니티
+              </button>
+            </li>
+            <li className="navItem">
+              <button type="button" className="pathBtn" onClick={goTrainer}>
+                트레이너 매칭
+              </button>
+            </li>
+            <li className="navItem">
+              <button type="button" className="pathBtn" onClick={goSubScribe}>
+                구독하기
+              </button>
+            </li>
           </ul>
           <LoginGroup
             isMouseIn={isMouseIn}
